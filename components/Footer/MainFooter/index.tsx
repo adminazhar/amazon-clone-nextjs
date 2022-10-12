043420287f01cdb5861fbb6244cfba5fc820f551
Azhar Khan <admin@azhark.com>
2022-10-13 02:20:29 +0530

    Add MainFooter Component

---
+++ components/Footer/MainFooter/index.tsx
@@ -0,0 +1,300 @@
+import React from "react";
+
+export const MainFooter = () => {
+  return (
+    <div className="bg-[#232F3E] text-white">
+      <div className="flex flex-wrap flex-row justify-around px-56 py-10">
+        <div>
+          <div className="font-bold py-2">Get to Know Us</div>
+          <div className="text-sm text-gray-300">
+            <ul>
+              <li className="pb-2">
+                <a href="https://www.amazon.jobs" className="">
+                  Careers
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="https://blog.aboutamazon.com/?utm_source=gateway&utm_medium=footer"
+                  className=""
+                >
+                  Blog
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="https://www.aboutamazon.com/?utm_source=gateway&utm_medium=footer"
+                  className=""
+                >
+                  About Amazon
+                </a>
+              </li>
+              <li className="pb-2">
+                <a href="https://www.amazon.com/ir" className="">
+                  Investor Relations
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="/gp/browse.html?node=2102313011&ref_=footer_devices"
+                  className=""
+                >
+                  Amazon Devices
+                </a>
+              </li>
+              <li className=" ">
+                <a href="https://www.amazon.science" className="">
+                  Amazon Science
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className="font-bold py-2">Make Money with Us</div>
+          <div className="text-sm  text-gray-300">
+            <ul>
+              <li className="pb-2">
+                <a
+                  href="https://services.amazon.com/sell.html?ld=AZFSSOA&ref_=footer_soa"
+                  className=""
+                >
+                  Sell products on Amazon
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="https://services.amazon.com/amazon-business.html?ld=usb2bunifooter&ref_=footer_b2b"
+                  className=""
+                >
+                  Sell on Amazon Business
+                </a>
+              </li>
+              <li className="pb-2">
+                <a href="https://developer.amazon.com" className="">
+                  Sell apps on Amazon
+                </a>
+              </li>
+              <li className="pb-2">
+                <a href="https://affiliate-program.amazon.com/" className="">
+                  Become an Affiliate
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="https://advertising.amazon.com/?ref=ext_amzn_ftr"
+                  className=""
+                >
+                  Advertise Your Products
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="/gp/seller-account/mm-summary-page.html?ld=AZFooterSelfPublish&topic=200260520&ref_=footer_publishing"
+                  className=""
+                >
+                  Self-Publish with Us
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="https://go.thehub-amazon.com/amazon-hub-locker"
+                  className=""
+                >
+                  Host an Amazon Hub
+                </a>
+              </li>
+              <li className="">
+                <span className="">›</span>
+                <a
+                  href="/b/?node=18190131011&ld=AZUSSOA-seemore&ref_=footer_seemore"
+                  className=""
+                >
+                  See More Make Money with Us
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className="font-bold py-2">Amazon Payment Products</div>
+          <div className="text-sm  text-gray-300">
+            <ul>
+              <li className="pb-2">
+                <a href="/dp/B07984JN3L?plattr=ACOMFO&ie=UTF-8" className="">
+                  Amazon Business Card
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="/gp/browse.html?node=16218619011&ref_=footer_swp"
+                  className=""
+                >
+                  Shop with Points
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="/gp/browse.html?node=10232440011&ref_=footer_reload_us"
+                  className=""
+                >
+                  Reload Your Balance
+                </a>
+              </li>
+              <li className=" ">
+                <a
+                  href="/gp/browse.html?node=388305011&ref_=footer_tfx"
+                  className=""
+                >
+                  Amazon Currency Converter
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className="font-bold py-2">Let Us Help You</div>
+          <div className="text-sm  text-gray-300">
+            <ul>
+              <li className="pb-2">
+                <a
+                  href="/gp/help/customer/display.html?nodeId=GDFU3JS5AL6SYHRD&ref_=footer_covid"
+                  className=""
+                >
+                  Amazon and COVID-19
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="https://www.amazon.com/gp/css/homepage.html?ref_=footer_ya"
+                  className=""
+                >
+                  Your Account
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="https://www.amazon.com/gp/css/order-history?ref_=footer_yo"
+                  className=""
+                >
+                  Your Orders
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="/gp/help/customer/display.html?nodeId=468520&ref_=footer_shiprates"
+                  className=""
+                >
+                  Shipping Rates & Policies
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="/gp/css/returns/homepage.html?ref_=footer_hy_f_4"
+                  className=""
+                >
+                  Returns & Replacements
+                </a>
+              </li>
+              <li className="pb-2">
+                <a href="/gp/digital/fiona/manage?ref_=footer_myk" className="">
+                  Manage Your Content and Devices
+                </a>
+              </li>
+              <li className="pb-2">
+                <a
+                  href="/gp/BIT/ref=footer_bit_v2_us_A0029?bitCampaignCode=A0029"
+                  className=""
+                >
+                  Amazon Assistant
+                </a>
+              </li>
+              <li className=" ">
+                <a
+                  href="/gp/help/customer/display.html?nodeId=508510&ref_=footer_gw_m_b_he"
+                  className=""
+                >
+                  Help
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="border-t border-gray-500"></div>
+      <div className="flex flex-wrap flex-col items-center text-white px-56 py-10">
+        <div className="flex flex-row gap-16">
+          <div>
+            <img src="/images/amazon.png" className="w-24" />
+          </div>
+          <div>
+            <select className="bg-[#232F3E] text-white">
+              <option>English</option>
+              <option>English</option>
+              <option>English</option>
+            </select>
+          </div>
+        </div>
+        <div className="pt-6">
+          <ul className="flex flex-row flex-wrap justify-center gap-2 text-gray-200 text-sm">
+            <li className="nav_first">
+              <a href="https://www.amazon.com.au/ref=footer_au">Australia</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.com.br/ref=footer_br">Brazil</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.ca/ref=footer_ca">Canada</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.cn/ref=footer_cn">China</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.fr/ref=footer_fr">France</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.de/ref=footer_de">Germany</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.it/ref=footer_it">Italy</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.co.jp/ref=footer_jp">Japan</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.com.mx/ref=footer_mx">Mexico</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.nl/ref=footer_nl">Netherlands</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.pl/ref=footer_pl">Poland</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.sg/ref=footer_sg">Singapore</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.es/ref=footer_es">Spain</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.com.tr/ref=footer_tr">Turkey</a>
+            </li>
+            <li>
+              <a href="https://www.amazon.ae/ref=footer_ae">
+                United Arab Emirates
+              </a>
+            </li>
+            <li>
+              <a href="https://www.amazon.co.uk/ref=footer_uk">
+                United Kingdom
+              </a>
+            </li>
+            <li className="nav_last">
+              <a href="https://www.amazon.com/ref=footer_us">United States</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
