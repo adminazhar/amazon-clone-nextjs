de2cb3686a0133c38f50f654f49ae23ea7583a61
Azhar Khan <admin@azhark.com>
2022-10-13 02:20:17 +0530

    Add Header Component

---
+++ components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MenuBar } from './MenuBar'
+import { TopNav } from './TopNav'
+
+export const Header = () => {
+  return (
+    <>
+        <TopNav></TopNav>
+        <MenuBar></MenuBar>
+    </>
+  )
+}
