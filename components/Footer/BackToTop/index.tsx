d4a61e8a3fa6c1f8437e4e68014d5937bd829622
Azhar Khan <admin@azhark.com>
2022-10-13 02:21:00 +0530

    Add BackToTop Component

---
+++ components/Footer/BackToTop/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const BackToTop = () => {
+  return (
+    <div>
+        <div className="bg-[#485769] py-4 text-white text-sm text-center">
+            Back to Top
+        </div>
+    </div>
+  )
+}
